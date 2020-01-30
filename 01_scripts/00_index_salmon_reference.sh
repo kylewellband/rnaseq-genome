db63bed22eafacb151d318780f535c1d9364c7f4
kylewellband <kyle.wellband@gmail.com>
2020-01-30 11:09:19 -0500

    Fix bug in Salmon index script

--- 01_scripts/00_index_salmon_reference.sh
+++ 01_scripts/00_index_salmon_reference.sh
@@ -7,6 +7,11 @@ NCPUS=4
 grep "^>" $GENOME | cut -d " " -f 1 > 02_reference/decoys.txt
 sed -i.bak -e 's/>//g' 02_reference/decoys.txt
 
-cat $TRANSCRIPTION $GENOME > 02_reference/gentrome.fasta
+if [ ! -e $GENOME || ! -e $TRANSCRIPTOME ];then
+    echo "Error: genome or transcriptome files not found!"
+    exit
+fi
+
+cat $TRANSCRIPTOME $GENOME > 02_reference/gentrome.fasta
 
 salmon index -t 02_reference/gentrome.fasta -d 02_reference/decoys.txt -p $NCPUS -i 02_reference/salmon_index
